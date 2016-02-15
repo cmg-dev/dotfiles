10921552c653d924a440d5fe05c98c5accb53264
Christoph <Christoph.Gnip@gmail.com>
2016-02-15 01:11:19 +0100

    Add vim solarized to the install script

--- install.sh
+++ install.sh
@@ -27,7 +27,9 @@ mkdir -p ~/.vim-tmp
 echo "Installing additional stuff"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+curl https://raw.githubusercontent.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+mv solarized.vim ~/.vim/colors/
+
 echo "Configuring zsh as default shell"
 chsh -s $(grep /zsh$ /etc/shells | tail -1)
-
-echo "Done."
+env zsh
