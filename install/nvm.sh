004611c5f1cb310e5a148710bde512147966e7ac
= <paul.heidenreich@5minds.de>
2017-06-12 11:08:07 +0200

    :bug: Fix nvm/node installation

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-# reload nvm into this environment
-source $(brew --prefix nvm)/nvm.sh
-
+# install script for nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
+# load nvm into current env
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+# install stable node version
 nvm install stable
 nvm alias default stable
