fff4c750e8ba03f22f77b5458e592209e6a5140c
= <paul.heidenreich@5minds.de>
2017-06-12 09:43:41 +0200

    :sparkles: Enable show percent of battery

--- install/osx.sh
+++ install/osx.sh
@@ -40,7 +40,7 @@ defaults write NSGlobalDomain AppleFontSmoothing -int 2
 #defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
 # Show remaining battery time; hide percentage
-# defaults write com.apple.menuextra.battery ShowPercent -string "NO"
+defaults write com.apple.menuextra.battery ShowPercent -string "NO"
 # defaults write com.apple.menuextra.battery ShowTime -string "YES"
 
 # echo "Always show scrollbars"
